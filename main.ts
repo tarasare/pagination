f14b64875549373529dd2e1c4a1e33d47520b57c
tarasare <sundayjune66@gmail.com>
2024-10-28 11:43:53 +0700

    add npx

--- main.ts
+++
@@ -1,8 +0,0 @@
-import { Sort } from './src/commons/sort';
-import { OrderBy } from './src/commons/orderby';
-import { Pageable } from './src/commons/pageable';
-import { Page } from './src/commons/page';
-import { PageRequest } from './src/commons/pagerequest';
-
-
-export { Sort , OrderBy, Pageable, Page, PageRequest }
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { Pageable } from './commons/pageable';
+import { Sort } from './commons/sort';
+import { OrderBy } from './commons/orderby';
+import { Page } from './commons/page';
+import { PageRequest } from './commons/pagerequest';
+
+
+export { Sort , OrderBy, Pageable, Page, PageRequest }
